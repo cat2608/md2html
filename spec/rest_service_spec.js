349b0c9eb31c39b2812e444e9efaa0f2b7249af5
Catalina <cat2608@gmail.com>
2015-04-28 08:38:53 +0700

    Test executed in sequence

--- spec/rest_service_spec.js
+++ spec/rest_service_spec.js
@@ -2,15 +2,21 @@ var frisby = require('frisby');
 
 frisby.create('Transform and store markdown to DB')
   .post('http://127.0.0.1:8888/markdown/save', {
-      text: "# MD2HTML"
-    })
-    .expectJSONTypes('html', {
+    text: "# MD2HTML"
+  }, {json: true})
+  .expectHeaderContains('content-type', 'application/json')
+  .expectStatus(200)
+  .expectJSONTypes('html', {
       text: String,
       id  : String
-    })
-.toss()
-
-frisby.create('Retrieve document from DB')
-  .get('http://127.0.0.1:8888/markdown/get?id=553e37ce1d6cfea6ef8b81a6')
-  .expectJSONTypes({ 'html': Object })
+  })
+  .afterJSON(function (document) {
+    var id = document.html.id;
+    frisby.create('Retrieve document from DB')
+      .get('http://127.0.0.1:8888/markdown/get?id='+ id)
+      .expectHeaderContains('content-type', 'application/json')
+      .expectStatus(200)
+      .expectJSONTypes({ 'html': Object })
+    .toss();
+})
 .toss();
